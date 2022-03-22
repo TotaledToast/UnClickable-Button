af05a79fca781eb729740c4d4716c49c84f0f21f
Toast <TotaledToast@gmail.com>
2022-03-22 20:29:41 +0000

    Added Comments

--- UnClickable Button/MainWindow.xaml.cs
+++ UnClickable Button/MainWindow.xaml.cs
@@ -29,19 +29,19 @@ namespace UnClickable_Button
         {
             Random Rand = new Random();
             int x, y;
-            Point MousePos;
-            bool WithinBox = true;
+            Point MousePos; //point variable is needed to store coordinate positions of objects
+            bool WithinBox = true;  //sets all variables
             do
             {
-                MousePos = Mouse.GetPosition(Window);
-                x = Rand.Next(0, Convert.ToInt32(Window.ActualWidth) - Convert.ToInt32(UnClickable.Width + 1));
+                MousePos = Mouse.GetPosition(Window);  //fetches coordinate position of cursor
+                x = Rand.Next(0, Convert.ToInt32(Window.ActualWidth) - Convert.ToInt32(UnClickable.Width + 1));     //sets x and y as a new random number between 0 and the size of the current window - size of button
                 y = Rand.Next(0, Convert.ToInt32(Window.ActualHeight) - Convert.ToInt32(UnClickable.Height + 1));
-                if (MousePos.X < x || MousePos.X > x + UnClickable.ActualWidth || MousePos.Y < y || MousePos.Y > y + UnClickable.ActualHeight)
+                if (MousePos.X < x || MousePos.X > x + UnClickable.ActualWidth || MousePos.Y < y || MousePos.Y > y + UnClickable.ActualHeight)  //checks if the current mouse position is inside where the button will be places, if so it tries again
                 {
                     WithinBox = false;
                 }
-            } while (WithinBox == true); ;
-            UnClickable.Margin = new Thickness(x,y,0,0);
+            } while (WithinBox == true);
+            UnClickable.Margin = new Thickness(x,y,0,0); //sets where the new button position is
         }
         
     }
